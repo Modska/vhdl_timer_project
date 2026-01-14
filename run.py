03cec61e3dc5677131501ff711a7208ef9a04446
Modska <renatoteixeira1205@gmail.com>
2026-01-14 17:05:04 +0100

    fix for GHDL version detection and CI

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from vunit import VUnit
+import os
 
-# Create VUnit instance
-vu = VUnit.from_argv()
+# Create VUnit instance with deprecated warnings fixed
+vu = VUnit.from_argv(compile_builtins=False)
+vu.add_vhdl_builtins()
 
 # Create a library named 'lib'
 lib = vu.add_library("lib")
